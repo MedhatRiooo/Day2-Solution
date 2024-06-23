1abe870413f3afb3a6c965e71b2608ed3ca97d25
MedhatRiooo <medhatqeriaqous@gmail.com>
2024-06-24 02:56:21 +0300

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region comments
+            //single line comment
+            /*
+             * multiple 
+             * line
+             * comment
+             * 
+             * 
+             * 
+             * */
+            #endregion
+
+            #region Variable Declaration
+            //int id = 10;
+
+            ////PascalCase -> FirstName
+            ////camelCase -> firstName
+            ////kabab-case -> first-name [Angular Files]
+            ////snake_case -> first_name
+            //Console.WriteLine(id);   
+            #endregion
+
+
+        }
+    }
+}

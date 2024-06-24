f7849fbb191cdacaae04ad1e9b1351db78f6b644
MedhatRiooo <medhatqeriaqous@gmail.com>
2024-06-24 14:50:43 +0300

    q2

---
+++ Assigment 1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assigment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //Console.Write("Enter a number: ");
+            //int Number = int.Parse(Console.ReadLine() ?? "0");
+            //Console.WriteLine($"The number is: {Number}"); 
+            #endregion
+
+            Console.Write("Enter a string to convert to an integer:");
+            string input = Console.ReadLine() ?? "NA";
+
+            int result;
+            bool flag = int.TryParse(input, out result);
+            Console.WriteLine(flag);
+            Console.WriteLine(result);
+        }
+    }
+}
